93d7a0327a87b2e0f38721118316eee8465a511c
曹小毛 <xiaomao.cao@renren-inc.com>
2014-01-20 11:51:17 +0800

    init

---
+++ studyBase/BIDAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  BIDAppDelegate.h
+//  studyBase
+//
+//  Created by renren on 14-1-20.
+//  Copyright (c) 2014年 renren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ studyBase/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  studyBase
+//
+//  Created by renren on 14-1-20.
+//  Copyright (c) 2014年 renren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
